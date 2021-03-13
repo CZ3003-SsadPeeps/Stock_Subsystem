4530499181dfb6fffec93fde88ae78e1bfadecb8
unknown <GOHT0032@e.ntu.edu.sg>
2021-03-13 12:14:28 +0800

    First commit to repo
    Still have to find way to duplicate stock graph scene. News management need code to link to db to change stock price and get stock name and content.

---
+++ Stock_subsystem/Assets/Scenes/Stock_Graph.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Stock_Graph : MonoBehaviour
+{
+    [SerializeField] private Sprite circleSprite;
+    private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
+    private RectTransform CompanyName;
+    // calls the code when played
+
+    private void Awake()
+    {   
+        //initializes containers, who are points on graph, and list of points and produces graph
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+
+        // change this to change company name and list of prices shown
+        CompanyName = graphContainer.Find("CompanyName").GetComponent<RectTransform>();
+        List<int> valueList = new List<int>() { 5, 98, 56, 45, 30, 22, 17, 15, 13, 17, 25 };
+        ShowGraph(valueList);
+    }
+
+    // used to create the points corresponding to the list
+    private GameObject CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    // Displays each point and connects them with a line
+    private void ShowGraph(List<int> valueList)
+    {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float xSize = 40f;
+        float yMaximum = 100f;
+        GameObject lastCircleGameObject = null; 
+
+        // This part of the code creates the dots, and connects them with a line
+        for (int i=0; i< valueList.Count; i++)
+        {
+            float xPosition = i * xSize;
+            float yPosition = (valueList[i] / yMaximum) *graphHeight;
+            GameObject circleGameObject =  CreateCircle(new Vector2(xPosition, yPosition));
+            if (lastCircleGameObject != null)
+            {
+                CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            lastCircleGameObject = circleGameObject;
+
+            // this part of the code insantiates the x-axis
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer.transform,false);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition = new Vector2(xPosition, 0f);
+            labelX.GetComponent<Text>().text = i.ToString();
+
+        }
+
+        // this part of the code insantiates the y-axis
+        int separatorCount = 10;
+        for(int i=0; i< separatorCount; i++)
+        {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer.transform, false);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = i * 1f / separatorCount;
+            labelY.anchoredPosition = new Vector2(-20f, normalizedValue * graphHeight);
+            labelY.GetComponent<Text>().text = Mathf.RoundToInt(normalizedValue *yMaximum).ToString();
+        }
+        // code to initiatlize the company name labelling
+
+            RectTransform CompanyName1 = Instantiate(CompanyName);
+            CompanyName1.SetParent(graphContainer.transform,false);
+            CompanyName1.gameObject.SetActive(true);
+            CompanyName1.GetComponent<Text>().text = "AAPL";
+    }
+
+    //code for creating the line connecting the dots
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1,1,1, .5f);
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0,GetAngleFromVectorFloat(dir));
+    }
+
+    //angle to change a vector to a floating number
+    public static float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+}
