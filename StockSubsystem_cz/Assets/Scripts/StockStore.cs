c509d036750f81388ca46128c8cb940a0bc387e2
unknown <GOHT0032@e.ntu.edu.sg>
2021-03-13 17:22:30 +0800

    Revert "2nd commit"
    This reverts commit fd602e7872565074b869776c39c4e91d27fb81cb.

---
+++ StockSubsystem_cz/Assets/Scripts/StockStore.cs
@@ -0,0 +1,17 @@
+﻿public class StockStore
+{
+    public static readonly Stock[] stocks = new Stock[]
+    {
+        new Stock("Tesla", 56),
+        new Stock("NIO Inc", 20),
+    };
+
+    public static int SelectedStockPos = -1;
+
+    public static Stock SelectedStock {
+        get
+        {
+            return stocks[SelectedStockPos];
+        }
+    }
+}
