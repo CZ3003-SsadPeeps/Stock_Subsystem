ee7a2929a4f6676bb0c3ad315e2e3becbcf4bcda
unknown <GOHT0032@e.ntu.edu.sg>
2021-03-13 12:22:10 +0800

    first commit
    Still need to work on duplicating stocks for the stock graphs. Need guidance on news page on how to link to db.

---
+++ StockSubsystem_cz/Assets/Scripts/Demo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Demo : MonoBehaviour
+{
+    [Serializable]
+    public struct Stock
+    {
+        public string Name;
+    }
+
+    [SerializeField] Stock[] allStocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject buttonTemplate = transform.GetChild(0).gameObject;
+        GameObject g;
+
+        int N = allStocks.Length;
+
+        for (int i = 0; i < N; i++)
+        {
+            g = Instantiate(buttonTemplate, transform);
+            g.transform.GetChild(0).GetComponent<Text>().text = allStocks[i].Name;
+        }
+
+        Destroy(buttonTemplate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ StockSubsystem_cz/Assets/Scripts/NewsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewsManager : MonoBehaviour
+{
+    public RectTransform NewsDisplayed;
+    public string newscontent;
+    public RectTransform BackGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //to wake up the newsdisplayed obj and assign the string to it
+        BackGround = transform.Find("BackGround").GetComponent<RectTransform>();
+        NewsDisplayed = BackGround.Find("NewsDisplayed").GetComponent<RectTransform>();
+        newscontent = "TSLA CARS ARE LITERALLY ON FIRE!.";
+        DisplayNews(newscontent);
+
+    }
+
+    //code to display news
+    public void DisplayNews(string newscontent)
+    {
+        RectTransform Newsdisplayed = Instantiate(NewsDisplayed);
+        Newsdisplayed.gameObject.SetActive(true);
+        Newsdisplayed.transform.SetParent(BackGround.transform, false);
+        Newsdisplayed.anchoredPosition = new Vector2(109f, 0f);
+        Newsdisplayed.GetComponent<Text>().text = newscontent;
+    }
+
+    // used to retrieve the news needed from the databse
+    // public string  RetrieveNews(){}
+
+    //used to change the stock pricing becasue of the news, use FluctuationRate for this 
+    //void ChangeStockPricing(FluctuationRate){}
+}
